d799450169f8c1df3bb93501b864e3c03608cff1
Luiserebii <luis@serebii.io>
2019-07-24 03:22:08 -0400

    Finish up writing prototype npmpublish.sh script

--- npmpublish.sh
+++ npmpublish.sh
@@ -3,13 +3,17 @@
 # Make temporary directory, hold returned filepath
 TEMPDIR="$(mktemp -d)"
 
-# Setup a trap to execute on EXIT
-trap "{ rm -rf $TEMPDIR }" EXIT
+# Setup a trap to clear tempdir and execute on EXIT
+trap "rm -rf $TEMPDIR" EXIT
 
 # Copy necessary files over
+cp package.json README.md LICENSE dist/* $TEMPDIR
 
 # Change directory into temporary directory
+cd $TMPDIR
 
 # npm publish
+npm publish
 
 # Cleanup temporary directory
+rm -rf $TEMPDIR
